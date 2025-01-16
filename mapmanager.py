f95dc8ffba49c280caa8e49cedd632ef373fcd6b
Milk <145270243+Milky47Way@users.noreply.github.com>
2025-01-16 19:10:58 +0200

    mapmanager.py

--- mapmanager.py
+++ mapmanager.py
@@ -1,37 +1,46 @@
-class Mapmanager:
-    import pickle
-    """Керування карткою"""
+import pickle
+class Mapmanager():
 
     def __init__(self):
-        self.model = 'block'  # модель кубика лежить у файлі block.egg
-        # використовуються такі текстури:
-        self.textures = [
-            "textures/red.jpg",
-            #"textures/orange.jpg",
-            "textures/pink.jpg",
-            #"textures/purple.jpg",
-            "textures/blue.jpg",
-           # "textures/white.jpg",
-        ]
+        self.blocks = {}
+
+        self.model = 'block'
 
         self.colors = [
             (0.2, 0.2, 0.35, 1),
-            (0.2, 0.5, 0.2, 1),
-            (0.7, 0.2, 0.2, 1),
-            (0.5, 0.3, 0.0, 1)
-        ]  # rgba
-        # створюємо основний вузол картки:
+            (0.2, 0.8, 0.35, 1)
+        ]
+        #self.texture = "grass.jpg"
+
+        self.textures = [
+            "textures/grass.jpg",
+            "textures/tree.png",
+            "textures/stone.png",
+        ]
+        # Визначаємо колір RGBA (червоний, зелений, синій, прозорість) для блоку.
+        # Це використовується для задання кольору поверх текстури.
+
+        # створюємо основний вузол мапи:
         self.startNew()
-        # Додамо структуру даних для збереження блоків
-        self.blocks = {}
+        # Викликаємо метод `startNew`, який створює базовий вузол для всієї мапи.
+
+        # створюємо будівельні блоки
+        self.addBlock((0, 10, 0))
+        # Додаємо один блок на мапу у позицію (x=0, y=10, z=0).
+        # Це координати у тривимірному просторі.
+
 
     def startNew(self):
-        """Створює основу для нової карти"""
-        self.land = render.attachNewNode("Land")  # вузол, до якого прив'язані всі блоки картки
-        self.blocks = {}  # Очищаємо список блоків
+
+
+
+        self.land = render.attachNewNode("Land")
+        # Створюємо вузол "Land" у сцені за допомогою `render.attachNewNode`.
+        # Усі блоки мапи будуть дочірніми до цього вузла.
+
 
     def getColor(self, z):
-        """Повертає колір для заданого рівня висоти"""
+
         if z < len(self.colors):
             return self.colors[z]
         else:
@@ -45,20 +54,32 @@ class Mapmanager:
             return self.textures[len(self.textures) - 1]
 
     def addBlock(self, position):
-        """Створює будівельні блоки"""
+        # Метод для додавання блоку до мапи.
+
         self.block = loader.loadModel(self.model)
+        # Завантажуємо модель блоку (файл block.egg).
+        # Цей метод шукає файл у папці ресурсів.
+
         self.block.setTexture(loader.loadTexture(self.getTexture(int(position[2]))))
+        # Задаємо текстуру для моделі блоку (завантажуємо файл block.png).
+
         self.block.setPos(position)
+        # Встановлюємо позицію блоку у просторі (x, y, z), передану через аргумент `position`.
         # self.color = self.getColor(int(position[2]))
+
         # self.block.setColor(self.color)
+        # Цей рядок відповідає за зміну кольору блоку поверх текстури,
+        # але його виконання зараз вимкнене.
+
         self.block.reparentTo(self.land)
-        # Додаємо блок до списку блоків (позиція як ключ, блок як значення)
-        self.blocks[position] = self.block
+        # Додаємо блок до вузла "Land", щоб він був частиною мапи.
+
 
     def clear(self):
         """Обнуляє карту"""
         self.land.removeNode()
         self.startNew()
+
 
     def loadLand(self, filename):
         """Створює карту землі з текстового файлу, повертає її розміри"""
@@ -75,6 +96,62 @@ class Mapmanager:
                 y += 1
         return x, y
 
+
     def isBlockAt(self, position):
         """Перевіряє, чи є блок у заданій позиції"""
         return position in self.blocks
+
+
+    def findBlocks(self, pos):
+        return self.land.findAllMatches('=at' + str(pos))
+
+
+    def findHighesEmpty(self, pos):
+        x, y, z = pos
+        z = 1
+        while self.isBlockAt((round(x), round(y), round(z))):
+            z += 1
+
+        return (round(x), round(y), z)
+
+
+    def buildBlock(self, pos):  # гравітація
+        x, y, z = pos
+        new = self.findHighestEmpty(pos)
+        if new[2] <= round(z) + 1:
+            self.addBlock(new)
+
+
+    def delBlock(self, position):
+        blocks = self.findBlocks(position)
+        for block in blocks:
+            block.removeNode()
+
+
+    def delBlockFrom(self, position):
+        x, y, z = self.findHighestEmpty(position)
+        pos = round(x), round(y), round(z) - 1
+        for block in self.findBlocks(pos):
+            block.removeNode()
+
+
+    def saveMap(self):
+        blocks = self.land.getChildren()
+        with open('my_map.dat', 'wb') as file:
+            pickle.dump(len(blocks), file)
+
+            for block in blocks:
+                x, y, z = block.getPos()
+                pos = int(x), int(y), int(z)
+                pickle.dump(pos, file)
+
+
+    def loadMap(self):
+        self.clear()
+
+
+        with open('my_map.dat', 'rb') as file:
+            length = pickle.load(file)
+            for i in range(length):
+                pos = pickle.load(file)
+                self.addBlock(pos)
